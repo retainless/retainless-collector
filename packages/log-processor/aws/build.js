9b20d4d1edf41ac982c2d55ca1438554322a7265
Hank Brekke-Peer <HankieCodes@users.noreply.github.com>
2025-10-26 13:06:38 -0500

    Licensing Documents

--- packages/log-processor/aws/build.js
+++ packages/log-processor/aws/build.js
@@ -1,6 +1,7 @@
 import * as Esbuild from "esbuild";
 import * as Path from "node:path";
 import * as ChildProcess from "node:child_process";
+import * as Fs from "node:fs/promises";
 
 await Esbuild.build({
     platform: "node",
@@ -24,6 +25,11 @@ await Esbuild.build({
     outdir: Path.join(import.meta.dirname, '..', 'build', 'bundles', 'aws'),
     logLevel: 'info'
 });
+
+await Fs.copyFile(
+    Path.join(import.meta.dirname, '..', 'LICENSE'),
+    Path.join(import.meta.dirname, '..', 'build', 'bundles', 'aws', 'LICENSE')
+)
 
 ChildProcess.execSync("zip -FS ../aws.zip *", {
     cwd: Path.join(import.meta.dirname, '..', 'build', 'bundles', 'aws'),
